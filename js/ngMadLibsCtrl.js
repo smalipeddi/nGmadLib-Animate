2d23eda807434bc68078f2e6db53eb8b8c5579f6
smalipeddi <sunitha_malipeddi@yahoo.com>
2015-06-07 17:16:44 -0400

    ngMadlibs enhanced

---
+++ js/ngMadLibsCtrl.js
@@ -0,0 +1,14 @@
+myapp = angular.module('MyApp',  ['ngAnimate']);
+myapp.controller('ngMadLibsCtrl', ['$scope', function($scope,$attrs) {
+  $scope.submitForm = function(isValid) {
+    if (isValid) {
+        $scope.myVar = !$scope.myVar;
+    }
+
+    
+
+  }
+
+
+
+}]);
